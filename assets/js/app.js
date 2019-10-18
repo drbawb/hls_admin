e95b8356e6e287fbbed82030459a924efe890cde
drbawb <drbawb@fatalsyntax.com>
2019-10-18 09:12:37 -0500

    (fix) issue w/ import syntax for socket

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,8 @@ import css from "../css/app.css"
 // Import dependencies
 //
 import "phoenix_html"
-import Socket from "phoenix"
-import LiveSocket from "phoenix_live_view"
+import {Socket} from "phoenix"
+import {LiveSocket} from "phoenix_live_view"
 
 
 // Import local files
